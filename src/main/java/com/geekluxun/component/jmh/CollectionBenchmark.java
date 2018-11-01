092c4b50655f7c25e280ddd51ad0aac083436ba6
luxun <geekluxun@163.com>
2018-11-01 10:09:41 +0800

    增加基准测试jmh

---
+++ src/main/java/com/geekluxun/component/jmh/CollectionBenchmark.java
@@ -0,0 +1,92 @@
+
+package com.geekluxun.component.jmh;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: luxun
+ * @Create: 2018-10-31 17:00
+ * @Description: 集合的基准测试示例
+ * @Other:
+ */
+@BenchmarkMode(Mode.AverageTime)      // 计算平均值
+@OutputTimeUnit(TimeUnit.NANOSECONDS) // 纳秒时间单位
+@Warmup(iterations = 5)
+public class CollectionBenchmark {
+
+    public static void main(String[] argc){
+        Options options = new OptionsBuilder()
+                .include(CollectionBenchmark.class.getSimpleName())
+                .threads(1)
+                .forks(1)
+                .shouldFailOnError(true)
+                .shouldDoGC(true)
+                .jvmArgs("-server")
+                .build();
+
+        try {
+            new Runner(options).run();
+        } catch (RunnerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 状态类
+     */
+    @State(Scope.Thread)
+    public static class MyState{
+        private Set<Employee> employeeSet = new HashSet<>();
+        private List<Employee> employeeList = new ArrayList<>();
+
+
+        private long iterations = 10000;
+
+        private Employee employee = new Employee(100L , "luxun");
+
+        /**
+         * 初始化
+         */
+        @Setup(Level.Trial)
+        public void setUp(){
+            for (long i = 0; i < iterations; i++){
+                employeeSet.add(new Employee(i, "geek"));
+                employeeList.add(new Employee(i, "geek"));
+            }
+
+            employeeList.add(employee);
+            employeeSet.add(employee);
+        }
+    }
+
+    /**
+     * 基准测试list
+     * @param state
+     * @return
+     */
+    @Benchmark
+    public boolean testArrayList(MyState state){
+        return  state.employeeList.contains(state.employee);
+    }
+
+    /**
+     * 基准测试set
+     * @param state
+     * @return
+     */
+    @Benchmark
+    public boolean testHashSet(MyState state){
+        return state.employeeSet.contains(state.employee);
+    }
+
+}
---
+++ src/main/java/com/geekluxun/component/jmh/Employee.java
@@ -0,0 +1,35 @@
+package com.geekluxun.component.jmh;
+
+import java.util.Objects;
+
+/**
+ * @Author: luxun
+ * @Create: 2018-10-31 17:02
+ * @Description:
+ * @Other:
+ */
+public class Employee {
+    private Long id;
+    private String name;
+
+    public Employee(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) &&
+                Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+}
